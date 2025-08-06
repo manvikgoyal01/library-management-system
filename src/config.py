9044df8271b3f189ad56f293bafb9969cc696fa1
Manvik Goyal <goyalmanvik01@gmail.com>
2025-08-06 19:57:57 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,8 +3,8 @@
 - !!! Ensure you do NOT change the type of the variable, ie pass string instead of integer or vice versa !!!
 '''
 
-lib_pass     = "lib123"
-borrow_limit = 5
-return_days  = 7
-match_limit  = 3
-match_cutoff = 75
+lib_pass     = "lib123"     #set centralised librarian password
+borrow_limit = 5            #max no. of books one user can have at any given time
+return_days  = 7            #number of days after issue date for return date to be set
+match_limit  = 3            #the number of similar results (above cutoff) displayed to user
+match_cutoff = 75           #the minimum match ratio for it to be considered 
